769495595d72c6d332b3ae0e2f148c0b28830d4e
Michael Hungbo <hungbomichael@gmail.com>
2022-06-12 05:15:14 +0100

    feat: configure mdx

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  compiler: {
-    styledComponents: true,
-  },
-}
+    compiler: {
+        styledComponents: true,
+    },
+};
 
-module.exports = nextConfig
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+        // If you use `MDXProvider`, uncomment the following line.
+        // providerImportSource: "@mdx-js/react",
+    },
+});
+
+module.exports = withMDX({
+    // Append the default value with md extensions
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    nextConfig,
+});
