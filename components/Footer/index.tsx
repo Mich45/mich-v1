151c48523920ebeb5b25d3cda1f8e4e3d78c98e6
Michael Hungbo <hungbomichael@gmail.com>
2022-07-02 14:32:38 +0100

    refactor: refactor footer styles

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,19 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+    Logo,
+    TwitterIcon,
+    GitHubIcon,
+    LinkedInIcon,
+    GmailIcon,
+} from '../Icons';
 
 const FooterWrapper = styled.footer`
-    height: 100px;
+    height: auto;
     width: 100%;
     display: flex;
     background: #e7cfcf61;
     display: flex;
+    flex-direction: column;
     position: relative;
     place-content: center;
     place-items: center;
+    margin: 10px 0;
+`;
+
+const SocialWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    place-items: center;
+    place-content: center;
+    margin: 10px;
 `;
 
 const Paragraph = styled.p`
     font-size: 14px;
+    margin-top: 5px;
+`;
+
+const Link = styled.a`
+    position: relative;
+    margin-inline: 10px;
+    width: 34px;
+    height: 34px;
+    border-radius: 50%;
+    background: #d7cccc;
+    display: flex;
+    place-content: center;
+    place-items: center;
+
+    &::after {
+        content: '';
+        width: 22px;
+        height: 22px;
+        background: #847a975e;
+        opacity: 0.5;
+        border-radius: 50%;
+        position: absolute;
+    }
+
+    :hover {
+        cursor: pointer;
+    }
 `;
 
 const Footer = (): JSX.Element => {
@@ -22,6 +66,22 @@ const Footer = (): JSX.Element => {
     return (
         <>
             <FooterWrapper>
+                <Logo />
+                <SocialWrapper>
+                    <Link>
+                        <GitHubIcon />
+                    </Link>
+                    <Link>
+                        <TwitterIcon />
+                    </Link>
+                    <Link>
+                        {' '}
+                        <LinkedInIcon />
+                    </Link>
+                    <Link>
+                        <GmailIcon />
+                    </Link>
+                </SocialWrapper>
                 <Paragraph>
                     Copyright © {date} Michael Hungbo. All rights reserved.
                 </Paragraph>
