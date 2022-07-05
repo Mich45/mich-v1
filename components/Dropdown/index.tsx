5086b3696edde21fe59783dae63c24847c768f13
Michael Hungbo <hungbomichael@gmail.com>
2022-07-05 17:05:24 +0100

    feat: implement sidebar

--- components/Dropdown/index.tsx
+++ components/Dropdown/index.tsx
@@ -1,11 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { MenuIcon, CloseIcon } from '../Icons';
 import Sidebar from '../Sidebar';
+import styled from 'styled-components';
+
+const Menu = styled.span`
+height: 2px;
+width: 16px;
+position: relative: 
+background: red;
+
+
+&:: before {
+    position: absolute;
+    content: '';
+    left: 0;
+    top: -5px;
+    transition: top, transform;
+    transition-duration: .1s;
+    transition-timing-function: ease-in-out;
+    top: 0;
+    transform: rotate(-45deg);
+}
+
+&:: after {
+    position: absolute;
+    content: '';
+    left: 0;
+    top: 5px;
+    transition: top, transform;
+    transition-duration: .1s;
+    transition-timing-function: ease-in-out;
+    top: 0;
+    transform: rotate(45deg);
+}
+
+`;
 
 const Dropdown = () => {
     const [isOpen, setIsOpen] = useState(true);
 
-    const handleCLick = (e) => {
+    const handleCLick = (e): void => {
         e.addEventListener('click', () => {
             setIsOpen(!isOpen);
         });
@@ -14,7 +48,7 @@ const Dropdown = () => {
     return (
         <>
             <MenuIcon onClick={handleCLick} />
-            <CloseIcon />
+            <CloseIcon onClick={handleCLick} />
 
             {isOpen ? <Sidebar /> : null}
         </>
