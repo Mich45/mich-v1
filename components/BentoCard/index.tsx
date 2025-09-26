929a441eda48d3a6689d88e59203552f8af1c29d
Michael Hungbo <hungbomichael@gmail.com>
2025-09-26 18:59:17 +0100

    refactor: refactor styles

--- components/BentoCard/index.tsx
+++ components/BentoCard/index.tsx
@@ -1,11 +1,17 @@
 import styled, { ThemedStyledProps } from 'styled-components';
 
+
+const Wrapper = styled.div`
+height: 400px;
+border: 1px solid rgb(22 86 69 / 99%);
+border-radius: 2px;
+`;
+
 const StyledBentoCard = styled.div`
   display: flex; 
   flex-direction: column; 
   justify-content: space-between;
-  border-radius: 0.1rem;
-  height: 300px;
+  height: 60%;
   box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
   padding: 2rem;
   font-family: 'Neue Montreal', sans-serif;
@@ -52,7 +58,7 @@ const StyledButton = styled.button<StyledButtonProps>`
     border: none;
     transition: background-color 0.3s ease;
     
-    ${(props: StyledButtonProps) => props.type === 'garden' && `
+    ${(props: StyledButtonProps) => props.type === 'digital-garden' && `
         background-color: #4a5568;
         color: #fff;
         &:hover {
@@ -60,13 +66,14 @@ const StyledButton = styled.button<StyledButtonProps>`
         }
     `}
 
-    ${(props: StyledButtonProps) => props.type === 'about' && `
-        background-color: #a0aec0;
+    ${(props: StyledButtonProps) => props.type === 'about-me' && `
+        background-color: #f6f7f8ff;
         color: #2d3748;
         &:hover {
-            background-color: #a0aec0;
+            background-color: #d5e1ddff;
         }
     `}
+
     ${(props: StyledButtonProps) => props.type === 'projects' && `
         background-color: #4a5568;
         color: #fff;
@@ -75,6 +82,29 @@ const StyledButton = styled.button<StyledButtonProps>`
         }
     `}
 `;
+
+interface IconDivProps {
+    type: string;
+}
+
+const IconDiv = styled.div<IconDivProps>`
+    width: 240px;
+    height: 40%;
+    background-repeat: no-repeat;
+
+    ${(props: IconDivProps) => props.type === 'digital-garden' && `
+        background-image: url('/assets/holding-pen.svg');
+    `}
+
+    ${(props: IconDivProps) => props.type === 'about-me' && `
+        background-image: url('/assets/person.svg');
+    `}
+    ${(props: IconDivProps) => props.type === 'projects' && `
+        background-image: url('/assets/hand-coding.svg');
+
+    `}
+
+  `;
 
 type BentoCardProps = {
   title: string;
@@ -85,6 +115,9 @@ type BentoCardProps = {
 
 const BentoCard = ({ title, subtitle, buttonText, type }: BentoCardProps) => {
   return (
+    <Wrapper>
+      <IconDiv type={type}>
+      </IconDiv>
     <StyledBentoCard className={type}>
       <div>
         <h3>{subtitle}</h3>
@@ -96,6 +129,7 @@ const BentoCard = ({ title, subtitle, buttonText, type }: BentoCardProps) => {
         </StyledButton>
       </div>
     </StyledBentoCard>
+    </Wrapper>
   );
 }
 
