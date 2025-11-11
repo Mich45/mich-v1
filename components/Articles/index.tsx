658af0e9e8b2ba9e8235ba49dfe735833265b426
Michael Hungbo <hungbomichael@gmail.com>
2025-11-11 14:41:19 +0100

    fix eslint error

--- components/Articles/index.tsx
+++ components/Articles/index.tsx
@@ -155,7 +155,7 @@ const ArticlesPreview = ({ meta }: PreviewProps) => {
                     .replace(/.md?./, '')}`}
                 passHref
             >
-                <ReadMore>Read more <span><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg></span></ReadMore>
+                <ReadMore>Read more <span><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg></span></ReadMore>
             </Link>
         </PreviewWrapper>
     );
